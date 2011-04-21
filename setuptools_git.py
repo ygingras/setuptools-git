16bf92c420ddc4ed1ca8f78a95ee1b580eb842e2
Wichert Akkerman <wichert@wiggy.net>
2011-04-21 11:00:41 +0200

    Use subprocess.check_output if available

--- setuptools_git.py
+++ setuptools_git.py
@@ -4,23 +4,39 @@
 A hook into setuptools for Git.
 """
 
-import os
-from subprocess import Popen, PIPE
+from subprocess import CalledProcessError
+from subprocess import PIPE
+
+try:
+    from subprocess import check_output
+except ImportError:
+    # BBB for python <2.7
+    def check_output(*popenargs, **kwargs):
+        from subprocess import Popen
+        if 'stdout' in kwargs:
+            raise ValueError('stdout argument not allowed, it will be overridden.')
+        process = Popen(stdout=PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise CalledProcessError(retcode, cmd, output=output)
+        return output
+
 
 def gitlsfiles(dirname=""):
     try:
-        p = Popen(['git', 'ls-files', dirname], stdout=PIPE, stderr=PIPE)
-        p.stderr.close()
-        files = p.stdout.readlines()
-    except:
+        output = check_output(['git', 'ls-files', dirname], stderr=PIPE)
+    except CalledProcessError:
         # Something went terribly wrong but the setuptools doc says we
         # must be strong in the face of danger.  We shall not run away
         # in panic.
         return []
-    if p.wait():
-        # git chocked
-        return []
-    return [f.strip() for f in files]
+
+    return output.splitlines()
+
 
 if __name__ == "__main__":
     import sys
